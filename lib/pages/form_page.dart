ec7e18ea71655fddfb45cb96dab3d9e75a8b491b
Jeisel Dias <jeisel@gmail.com>
2023-10-16 17:10:14 -0300

    Finalizando dia 16 de Outubro

---
+++ lib/pages/form_page.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+import 'package:imc/pages/result_page.dart';
+
+class FormPage extends StatefulWidget {
+  const FormPage({super.key});
+
+  @override
+  State<FormPage> createState() => _FormPageState();
+}
+
+class _FormPageState extends State<FormPage> {
+  int alturaEscolhida = 175;
+  int pesoEscolhido = 80;
+
+  void handleButton() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => const ResultPage()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          padding: const EdgeInsets.symmetric(vertical: 48),
+          margin: const EdgeInsets.fromLTRB(0, 0, 0, 16),
+          color: const Color(0xFF1D1F30),
+          child: Column(
+            children: [
+              const Text("Altura",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.grey)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(alturaEscolhida.toString(),
+                      style: const TextStyle(
+                          fontSize: 60,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white)),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                    child: Text("cm",
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey)),
+                  ),
+                ],
+              ),
+              Slider(
+                  min: 120,
+                  max: 230,
+                  value: double.parse(alturaEscolhida.toString()),
+                  onChanged: (double value) {
+                    setState(() {
+                      alturaEscolhida = value.round();
+                    });
+                  }),
+            ],
+          ),
+        ),
+        Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(vertical: 48),
+            color: const Color(0xFF1D1F30),
+            child: Column(children: [
+              const Text("Peso",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.grey)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(pesoEscolhido.toString(),
+                      style: const TextStyle(
+                          fontSize: 60,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white)),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                    child: Text("kg",
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey)),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        pesoEscolhido++;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      backgroundColor: const Color(0xff4D4E5D),
+                    ),
+                    child: const Text(
+                      "+",
+                      style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        pesoEscolhido--;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      backgroundColor: const Color(0xff4D4E5D),
+                    ),
+                    child: const Text(
+                      "-",
+                      style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  )
+                ],
+              ),
+            ])),
+        const SizedBox(
+          height: 16,
+        ),
+        Container(
+          width: double.infinity,
+          child: TextButton(
+            onPressed: handleButton,
+            style: TextButton.styleFrom(
+                backgroundColor: const Color(0xffD83459),
+                foregroundColor: Colors.white),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              child: Text(
+                "Calcular",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
